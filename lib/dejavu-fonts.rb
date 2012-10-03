7309061d3b39f75bad250f726e27c386dead84fb
Ayose <ayosec@gmail.com>
2012-10-03 05:48:43 +0100

    Weird SIGFAULT with the old implementation

--- lib/dejavu-fonts.rb
+++ lib/dejavu-fonts.rb
@@ -1,10 +1,13 @@
 module DejaVu
   module Fonts
-    Paths = {
-      :normal      => "#{File.dirname(__FILE__)}/fonts/DejaVuSans.ttf",
-      :italic      => "#{File.dirname(__FILE__)}/fonts/DejaVuSans-Oblique.ttf",
-      :bold        => "#{File.dirname(__FILE__)}/fonts/DejaVuSans-Bold.ttf",
-      :bold_italic => "#{File.dirname(__FILE__)}/fonts/DejaVuSans-BoldItalic.ttf"
-    }.freeze
+    extend self
+    def paths
+      {
+        :normal      => "#{File.dirname(__FILE__)}/fonts/DejaVuSans.ttf",
+        :italic      => "#{File.dirname(__FILE__)}/fonts/DejaVuSans-Oblique.ttf",
+        :bold        => "#{File.dirname(__FILE__)}/fonts/DejaVuSans-Bold.ttf",
+        :bold_italic => "#{File.dirname(__FILE__)}/fonts/DejaVuSans-BoldItalic.ttf"
+      }
+    end
   end
 end
